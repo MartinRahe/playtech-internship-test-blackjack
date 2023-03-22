fa4c0139bd26c68f72c514a821153e21d71ceb6c
MartinRahe <martin.r.rahe@gmail.com>
2023-03-22 15:12:52 +0200

    implemented checking for all listed fault options

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # playtech-internship-test-blackjack
 
 ## Possible cases of faulty moves:
-- [ ] a card appears twice
-- [ ] action after player bust is anything but "P Lose"
-- [ ] after revealing, dealer stops hitting before reaching 17
-- [ ] after revealing, dealer continues hitting after reaching 17
+- [x] a card appears twice
+- [x] action after player bust is anything but "P Lose"
+- [x] after revealing, dealer stops hitting before reaching 17
+- [x] after revealing, dealer continues hitting after reaching 17
 - [x] player hits after standing
 - [x] dealer reveals before player stands
-- [ ] player loses if dealer busts
-- [ ] lower hand total wins
-- [ ] dealer wins on equal hands
+- [x] player loses if dealer busts
+- [x] lower hand total wins
+- [x] dealer wins on equal hands
 - [x] same session is played again after player leaves
 - [x] player joins a session twice
+- [x] dealer starts with both cards hidden, both cards revealed, or a number of cards differing from two
+- [x] player does not start with two face up cards
+- [x] a hit does not affect the correct card count
--- src/main/java/martinrahe/playtech_internship_test_blackjack/Simulate.java
+++ src/main/java/martinrahe/playtech_internship_test_blackjack/Simulate.java
@@ -1,9 +1,6 @@
 package main.java.martinrahe.playtech_internship_test_blackjack;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 public class Simulate {
     public static List<String[]> simulateGame(List<String[]> gameData) {
@@ -13,12 +10,14 @@ public class Simulate {
         }
         int previousID = Integer.parseInt(gameData.get(0)[1]) - 1; //initialize to a lower value than any existing ID
         boolean faultFoundAtId = false; //used to skip over turns with the same id where a faulty move was made
-        System.out.println(previousID);
         int gameID;
         String action;
         HashSet<String> allowedActions = new HashSet<>();
+        int playerCardCount = 0;
+        int dealerCardCount = 0;
+        String[] previousTurn = new String[0];
+
         for (String[] g : gameData) {
-            System.out.println(Arrays.toString(g));
             gameID = Integer.parseInt(g[1]);
             if (gameID == previousID && faultFoundAtId) {
                 continue;
@@ -31,51 +30,265 @@ public class Simulate {
                 previousID = gameID;
             }
 
+            String playerCards = g[5].strip();
+            String dealerCards = g[4].strip();
+            int playerSum = computeCardSum(playerCards);
+            int dealerSum = computeCardSum(dealerCards);
+            if (playerSum > 21) {
+                allowedActions.clear();
+                allowedActions.add("P Lose");
+                allowedActions.add("P Left");
+                allowedActions.add("D Redeal");
+            }
+
+            if (dealerSum > 21) {
+                allowedActions.clear();
+                allowedActions.add("P Win");
+                allowedActions.add("P Left");
+                allowedActions.add("D Redeal");
+            }
+
             action = g[3].strip();
+
             if (!allowedActions.contains(action)) {
                 faultyMoves.add(g);
                 faultFoundAtId = true;
                 continue;
             }
+
             if (action.equals("P Joined")) {
+                if (cardCount(dealerCards) != 2 || cardCountRevealed(dealerCards) != 1 ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != 2 ||
+                        cardCountRevealed(playerCards) != 2 || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is a not valid starting position
+                    faultyMoves.add(g);
+                    faultFoundAtId = true;
+                    continue;
+                }
                 allowedActions.remove("P Joined");
                 allowedActions.add("P Hit");
                 allowedActions.add("P Stand");
                 allowedActions.add("P Left");
+                playerCardCount = 2;
+                dealerCardCount = 2;
+                previousTurn = g;
+                continue;
+            }
+            if (action.equals("P Hit")) {
+                if (cardCount(dealerCards) != 2 || cardCountRevealed(dealerCards) != 1 ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != playerCardCount ||
+                        cardCountRevealed(playerCards) != playerCardCount || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is not what it should be based on the last move
+                    faultyMoves.add(previousTurn);
+                    faultFoundAtId = true;
+                    continue;
+                }
+                playerCardCount++;
+                previousTurn = g;
+                continue;
             }
             if (action.equals("P Stand")) {
+                if (cardCount(dealerCards) != 2 || cardCountRevealed(dealerCards) != 1 ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != playerCardCount ||
+                        cardCountRevealed(playerCards) != playerCardCount || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is not what it should be based on the last move
+                    faultyMoves.add(previousTurn);
+                    faultFoundAtId = true;
+                    continue;
+                }
                 allowedActions.remove("P Hit");
                 allowedActions.remove("P Stand");
                 allowedActions.add("D Show");
+                previousTurn = g;
+                continue;
             }
             if (action.equals("D Show")) {
+                if (cardCount(dealerCards) != 2 || cardCountRevealed(dealerCards) != 1 ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != playerCardCount ||
+                        cardCountRevealed(playerCards) != playerCardCount || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is not what it should be based on the last move
+                    faultyMoves.add(previousTurn);
+                    faultFoundAtId = true;
+                    continue;
+                }
                 allowedActions.remove("D Show");
                 allowedActions.add("D Hit");
                 allowedActions.add("P Win");
                 allowedActions.add("P Lose");
+                previousTurn = g;
+                continue;
+            }
+            if (action.equals("D Hit")) {
+                if (cardCount(dealerCards) != dealerCardCount || cardCountRevealed(dealerCards) != dealerCardCount ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != playerCardCount ||
+                        cardCountRevealed(playerCards) != playerCardCount || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is not what it should be based on the last move
+                    faultyMoves.add(previousTurn);
+                    faultFoundAtId = true;
+                    continue;
+                }
+                if (dealerSum > 16) {
+                    faultyMoves.add(g);
+                    faultFoundAtId = true;
+                    continue;
+                }
+                dealerCardCount++;
+                previousTurn = g;
+                continue;
             }
             if (action.equals("P Win")) {
+                if (cardCount(dealerCards) != dealerCardCount || cardCountRevealed(dealerCards) != dealerCardCount ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != playerCardCount ||
+                        cardCountRevealed(playerCards) != playerCardCount || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is not what it should be based on the last move
+                    faultyMoves.add(previousTurn);
+                    faultFoundAtId = true;
+                    continue;
+                }
+                if (dealerSum < 17 || playerSum < dealerSum) {
+                    faultyMoves.add(g);
+                    faultFoundAtId = true;
+                    continue;
+                }
                 allowedActions.clear();
                 allowedActions.add("P Left");
                 allowedActions.add("D Redeal");
+                previousTurn = g;
+                continue;
             }
             if (action.equals("P Lose")) {
+                if (cardCount(dealerCards) != dealerCardCount || cardCountRevealed(dealerCards) != dealerCardCount ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != playerCardCount ||
+                        cardCountRevealed(playerCards) != playerCardCount || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is not what it should be based on the last move
+                    faultyMoves.add(previousTurn);
+                    faultFoundAtId = true;
+                    continue;
+                }
+                if (dealerSum < 17 || dealerSum <= playerSum) {
+                    faultyMoves.add(g);
+                    faultFoundAtId = true;
+                    continue;
+                }
                 allowedActions.clear();
                 allowedActions.add("P Left");
                 allowedActions.add("D Redeal");
+                previousTurn = g;
+                continue;
             }
             if (action.equals("D Redeal")) {
+                if (cardCount(dealerCards) != 2 || cardCountRevealed(dealerCards) != 1 ||
+                        !allCardsValid(dealerCards) || cardCount(playerCards) != 2 ||
+                        cardCountRevealed(playerCards) != 2 || !allCardsValid(playerCards) ||
+                        containDuplicateCards(playerCards, dealerCards)) {
+                    //the set of cards on the table is a not valid starting position
+                    faultyMoves.add(g);
+                    faultFoundAtId = true;
+                    continue;
+                }
                 allowedActions.clear();
                 allowedActions.add("P Hit");
                 allowedActions.add("P Stand");
                 allowedActions.add("P Left");
+                playerCardCount = 2;
+                dealerCardCount = 2;
+                previousTurn = g;
+                continue;
             }
             if (action.equals("P Left")) {
                 allowedActions.clear();
             }
-            System.out.printf("%d %s\n", gameID, action);
         }
 
         return faultyMoves;
+    }
+
+    private static int computeCardSum(String cardsString) {
+        Map<String, Integer> cardValues  = Map.of("A", 11, "J", 10, "Q", 10, "K", 10, "?", 0);
+        String[] cards = cardsString.toUpperCase().strip().split("-");
+        int sum = 0;
+        String cardValue;
+        for (String c : cards) {
+            if (c.equals("?")) {
+                continue;
+            }
+            cardValue = c.substring(0,c.length()-1);
+            if (cardValues.containsKey(cardValue)) {
+                sum += cardValues.get(cardValue);
+                continue;
+            }
+            sum += Integer.parseInt(cardValue);
+        }
+        return sum;
+    }
+
+    private static int cardCount(String cardsString) {
+        String[] cards = cardsString.toUpperCase().strip().split("-");
+        return cards.length;
+    }
+
+    private static int cardCountRevealed(String cardsString) {
+        String[] cards = cardsString.toUpperCase().strip().split("-");
+        int count = 0;
+        for (String c : cards) {
+            if (c.equals("?")) {
+                continue;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    private static boolean allCardsValid(String cardsString) {
+        String[] cards = cardsString.toUpperCase().strip().split("-");
+        String type;
+        String cardValue;
+        for (String c : cards) {
+            if (c.equals("?")) {
+                continue;
+            }
+            if (c.length() < 2) {
+                return false;
+            }
+            type = c.substring(c.length()-1);
+            cardValue = c.substring(0,c.length()-1);
+            if (!"SHCD".contains(type)) {
+                return false;
+            }
+            if (!"AJQK".contains(cardValue)) {
+                try {
+                    int num = Integer.parseInt(cardValue);
+                    if (num < 2 || num > 10) {
+                        return false;
+                    }
+                } catch (NumberFormatException e) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean containDuplicateCards(String playerCards, String dealerCards) {
+        String cardsString = playerCards.strip() + "-" + dealerCards.strip();
+        String[] cards = cardsString.toUpperCase().split("-");
+        HashSet<String> cardSet = new HashSet<>();
+        for (String c : cards) {
+            if (c.equals("?")) {
+                continue;
+            }
+            if (cardSet.contains(c)) {
+                return true;
+            }
+            cardSet.add(c);
+        }
+        return false;
     }
 }
